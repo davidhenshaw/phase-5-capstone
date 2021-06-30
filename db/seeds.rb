57bb2d398401e2e33e9d99fa70439ce32fda32c1
davidhenshaw <davidhenshaw2014@gmail.com>
2021-06-30 10:36:58 -0500

    seed database with local images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'base64'
+
+puts "Encoding asset files..."
+imgs = ["admiral.png", "dog.png", "eminem_face.png"]
+encodedImgs = []
+
+# populate the encodedImgs array with each image encoded to base64
+imgs.each do |img|
+    # generate absolute path to the image asset 
+    # otherwise I'd have to enter the entire absolute path for every image
+    path = File.join(File.dirname(__FILE__), "/assets/#{img}")
+
+    # encode the image to base64 and put into array
+    encodedImgs << Base64.encode64(File.open(path, 'rb').read)
+end
+
 puts "Clearing existing data..."
 User.destroy_all
 
 puts "Inserting data..."
-user = User.create(username: "David");
+user = User.create(username: "David", avatar:encodedImgs[0]);
+user2 = User.create(username: "Rowland", avatar:encodedImgs[1]);
+user3 = User.create(username: "Declan", avatar:encodedImgs[2]);
 
 puts "Done."
