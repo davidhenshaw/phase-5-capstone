aab473dfeb6153a7c3f9589af7bdbcc87e92e53f
davidhenshaw <davidhenshaw2014@gmail.com>
2021-07-01 11:50:52 -0500

    seed user data

--- db/migrate/20210701161038_create_users.rb
+++ db/migrate/20210701161038_create_users.rb
@@ -1,6 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :display_name
+      t.binary :avatar
+      t.text :bio
+      
+      t.string :email
+      t.string :twitter_name
+      t.string :instagram_name
+      t.string :youtube_name
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Clearing existing data..."
+User.destroy_all
+
+puts "Generating users..."
+
+5.times do |i|
+    display_name = Faker::Name.name
+    first_name = display_name.split(/[ -.]/).first
+    last_name = display_name.split(/[ -.]/).last
+    
+    username = Faker::Internet.user_name(display_name, %w(. _ -) )
+    password = Faker::Internet.password(8)
+    email = Faker::Internet.safe_email(first_name)
+
+    user = User.create(display_name: display_name, username: username, password: password, email: email)
+end
+
+puts "Done."
