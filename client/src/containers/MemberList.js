c0bb14718d98d09f7e202283988352f85220a0a2
davidhenshaw <davidhenshaw2014@gmail.com>
2021-07-06 17:18:15 -0500

    add members

--- client/src/containers/MemberList.js
+++ client/src/containers/MemberList.js
@@ -1,13 +1,28 @@
 import { React } from "react";
 import UserSearchForm  from "../components/UserSearchForm";
+import axios from "axios";
 
 function MemberList(props) {
     let { members , onMemberAdd, onMemberRemove, project } = props;
 
+    function handleMemberRemove(member)
+    {
+        let config = {
+            headers:{
+                Authorization: `Bearer ${localStorage.token}`
+            }
+        }
+        
+        axios.delete(`/members/${member.id}`, config)
+        .then( (res) => {
+            console.log(res)
+            onMemberRemove(member)
+        });
+    }
 
     return(
         <div>
-            {members.map((member, idx) => <MemberCard onMemberRemove={onMemberRemove} member={member} key={idx}/>)}
+            {members.map((member, idx) => <MemberCard onMemberRemove={handleMemberRemove} member={member} key={idx}/>)}
             <UserSearchForm 
                 onMemberAdd={onMemberAdd}
                 onMemberRemove={onMemberRemove}
