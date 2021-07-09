1bcd1400436a119950d4b641237ffec9dc30c4f4
davidhenshaw <davidhenshaw2014@gmail.com>
2021-07-09 11:08:46 -0500

    project delete button

--- client/src/containers/ProjectPage.js
+++ client/src/containers/ProjectPage.js
@@ -5,16 +5,18 @@ import {
 } from "react";
 
 import {
+    useHistory,
     useParams
 } from "react-router-dom";
 
 import axios from 'axios';
 import MemberList from "./MemberList";
-import UserSearchForm from "../components/UserSearchForm";
+import { Button } from "@material-ui/core";
 
 function ProjectPage(props)
 {
     let {id} = useParams();
+    const history = useHistory();
     const [project, setProject] = useState({})
     const [isLoading, setIsLoading] = useState(true);
 
@@ -53,6 +55,26 @@ function ProjectPage(props)
             "members": newList
         })
     }
+
+    function handleDelete()
+    {
+        if( !window.confirm(`Are you sure you want to delete ${project.name} for all members? There's no going back!`))
+            return;
+
+        let config = {
+            headers:{
+                Authorization: `Bearer ${localStorage.token}`
+            }
+        }
+
+        axios.delete(`/projects/${project.id}`, config)
+        .then( res => {
+            history.push("/");
+            console.log(res);
+        }).catch( err =>{
+            console.log(err);
+        })
+    }
     
     return(
         <div>
@@ -68,6 +90,15 @@ function ProjectPage(props)
                     onMemberAdd={handleMemberAdd}
                     onMemberRemove={handleMemberRemove}
                     members={project.members} /> 
+            }
+            <br></br>
+            <br></br>
+            <br></br>
+            {
+                project.is_member ?
+                <Button variant="outlined" onClick={handleDelete}>Delete Project</Button>
+                :
+                null
             }
         </div>
     )
