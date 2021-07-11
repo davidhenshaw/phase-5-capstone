bdfb3a0ed9f066c765abc2b1ea9ea127e65f7289
davidhenshaw <davidhenshaw2014@gmail.com>
2021-07-11 10:01:48 -0500

    serialize user before render

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,8 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :user, :project, :header, :message
+
+  def user
+    user = User.find(self.object.user_id)
+    UserSerializer.new(user)
+  end
 end
